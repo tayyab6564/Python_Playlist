6b13bd3c345e132678c8d0dd2cad632e27db53f6
tayyab6564 <asadali6564265642@gmail.com>
2026-06-09 08:03:26 -0700

    1

---
+++ input output file.py
@@ -0,0 +1,132 @@
+# 1.r  -->  read       
+# 2.a  -->  append(add at last)    
+# 3.w  -->  write(overwrite and delete previous data)
+# 4.r+ -->  read+write(while writing it starts overwriting from front of file and start reading from next place from currently written text )
+# 5.w+ --> write+read(file will open and all data will be erased)
+# 6.a+ --> append plus(data do not truncate, can read data and can write at end)
+# 7. with syntex (it automatically close file do not need manually to close a file)
+# 8.import os  os.remove --> delete file
+# 9. replace(to be replaced , which will be replaced)
+
+                #  1.read data from file
+"""
+f = open("file.txt","r")#use file name if file is in same folder
+#f = open("D:\Tayyab\io file.txt","r") #use file location when file is in another folder
+data = f.read()
+print("data in file is: \n \t",data)
+"""
+
+#read only some piece of initial data  :first eight characters
+"""
+f = open("file.txt","r")
+data = f.read(8)
+print("\nfirst eight characters: \n \t",data)
+
+#print one line
+#f = open("file.txt","r")
+data = f.readline()
+print("\nfirst line is: \n \t",data)
+f.close()
+"""
+
+#                2.write into file 
+"""
+f = open("file.txt","w")
+f.write("todary i am learning python. \nTomorrow i will learn javascript.")
+f.close()
+"""
+
+#              3.append data(add dta at the last of a file)
+"""
+f = open("file.txt","a")
+f.write("\nThis data is appended and is added to last of the file")
+f.close()
+"""
+
+#                   4. read + write
+"""
+f = open("file.txt","r+")
+f.write("start")
+data1 = f.read()
+print(data1)
+f.close()
+"""
+
+#                   5. write + read
+"""
+f = open("file.txt","w+")
+data = f.read()
+print("\ndata in file is: \n",data)
+f.write("Python is easy")
+f.close()
+f = open("file.txt","r")
+data = f.read()
+print("data in file is: \n",data)
+f.close()
+"""
+
+#               6. append plus
+"""
+f = open("file.txt","a+")
+data = f.read() #pointer is at end so no data will print
+print("data in file is: \n",data)
+f.write(" writing at end")
+f.close()
+"""
+
+#            7. with syntax
+"""
+with open("file.txt","a+") as f:  # "as" mean aliance
+ f.write("\nThis is next line")
+
+with open("file.txt","r+") as f:  # "as" mean aliance
+ data = f.read()
+print("data in file is: \n",data)
+"""
+
+#             8. delete a file
+"""
+import os 
+os.remove("File.txt")
+"""
+
+#      replace(new , old)
+"""
+with open("file.txt","r+") as f: 
+ data = f.read()
+ data1 = data.replace("ali" , "umar")
+
+with open("file.txt","r+") as f: 
+ f.write(data1)
+print("data in file is: \n",data1)
+"""
+
+#find a world in file
+"""
+def find_word():
+ with open("file.txt","r") as f: 
+  word = input("Enter world to find from file: ")
+  data = f.read()
+  if(data.find(word) != -1):
+   print("present")
+  else:
+   print("Not found")
+find_word()
+"""
+
+#find line of first occurance of a number
+def find_word():
+ with open("file.txt","r") as f: 
+   word = input("Enter world to find from file: ")
+   line = 1
+   while True:
+    data = f.readline()
+    if(word in data):
+     print(line)
+     return
+    line+=1
+    if(data == ""):
+     print("not found")
+     return
+
+find_word()
--- recursion.py
+++ recursion.py
@@ -10,6 +10,7 @@ counting(50)
     """
 
 #Print factorial using recursion
+"""
 num = int(input("Enter number to print factorial: "))
 def factorial(n):
    if(n == 1 or n == 0):
@@ -17,4 +18,22 @@ def factorial(n):
    else:
       return n * factorial(n - 1)
    
-print(factorial(num))   
+print(factorial(num))   """
+
+#sum of n natural numbers
+"""
+num = int(input("Ending of natural numbers to add: "))
+def add(n):
+    if(n == 0):
+     return 0
+    return n + add(n-1)    
+print(add(num))    
+"""
+#print all elements in a list hint:pass list and index as parameters
+list = [1,"banana",2.0,"apple"]
+def printlist(lis , idx=0):
+   if(idx == len(list)):
+      return 
+   print(lis[idx])
+   printlist(lis , idx+1)
+printlist(list)
